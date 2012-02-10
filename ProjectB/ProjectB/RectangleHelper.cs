9109c053b7b0bce913457a636b3fe3e77044a7df
Jason Spafford <nullprogrammer@gmail.com>
2012-02-09 23:22:58 -0500

    Added a rectangle helper
    - Added a debug font to the content

---
+++ ProjectB/ProjectB/RectangleHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace ProjectB
+{
+	public static class RectangleHelpers
+	{
+		public static Rectangle FromVectors (Vector2 one, Vector2 two)
+		{
+			if (one.X == two.X || one.Y == two.Y)
+				return Rectangle.Empty;
+
+			Vector2 topLeft = one.X < two.X && one.Y < two.Y ? one : two;
+			Vector2 bottomRight = one.X > two.X && one.Y > two.Y ? one : two;
+
+			if (topLeft == two && bottomRight == two)
+			{
+				topLeft = new Vector2(one.X, two.Y);
+				bottomRight = new Vector2 (two.X, one.Y);
+			}
+
+			return new Rectangle ((int)topLeft.X, (int)topLeft.Y, (int)(bottomRight.X - topLeft.X), (int)(bottomRight.Y - topLeft.Y));
+		}
+
+		public static Rectangle ToRectangle(this Vector2 self, int width, int height)
+		{
+			return new Rectangle((int)self.X, (int)self.Y, width, height);
+		}
+	}
+}
