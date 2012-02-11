b28eed74bf0a6cdae2c34a638bb5da651853bda6
Jason Spafford <nullprogrammer@gmail.com>
2012-02-10 20:04:17 -0500

    Added TextureHelper

---
+++ ProjectB/ProjectB/TextureHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ProjectB
+{
+	public static class TextureHelpers
+	{
+		public static Vector2 Origin (this Texture2D self)
+		{
+			return new Vector2(self.Width / 2, self.Height / 2);
+		}
+	}
+}
